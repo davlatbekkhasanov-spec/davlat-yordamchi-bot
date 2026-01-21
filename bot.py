a3f1fb7005e93d979bc16772623a22003713c2b6
davlatbekkhasanov-spec <davlatbek.khasanov@icloud.com>
2026-01-22 00:50:13 +0500

    Refactor to load bot token from .env file
    Load bot token from environment variables using dotenv.

--- bot.py
+++ bot.py
@@ -3,7 +3,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-TOKEN = "BU_YERGA_BOT_TOKENING"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
