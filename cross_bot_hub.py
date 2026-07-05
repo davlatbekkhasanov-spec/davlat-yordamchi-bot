1f904322822ceedbb8d57a889f977f5d30086093
davlatbekkhasanov-spec <davlatbek.khasanov@icloud.com>
2026-07-05 16:32:04 +0500

    Revert "Register anonim bot events in the cross-bot hub and scoring."
    This reverts commit 02f98af35e2340a02e1af609ea1d22e65be97014.

--- cross_bot_hub.py
+++ cross_bot_hub.py
@@ -30,7 +30,6 @@ BOT_LABELS = {
     "inventarizatsiya": "Inventarizatsiya",
     "navbatchi": "Navbatchi nazorat",
     "faceid": "Face ID davomat",
-    "anonim": "Anonim murojaat",
 }
 
 _BOT_KEY_ALIASES = {
@@ -55,14 +54,6 @@ _BOT_KEY_ALIASES = {
         "navbatchi_nazorat",
     },
     "faceid": {"faceid", "face_id", "face-id", "faceidbot", "davomat"},
-    "anonim": {
-        "anonim",
-        "anonymous",
-        "anonimous",
-        "anonnymous",
-        "anonim_uz",
-        "anonimuz",
-    },
 }
 
 _PERSIST = bootstrap_persistence(DB_PATH, legacy_names=("data.db",))
